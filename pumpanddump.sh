3fad15510da184dc60d4b44d4c9574de9ee6b213
zenjabba <zenjabba@users.noreply.github.com>
2020-12-26 09:26:12 -0500

    Create pumpanddump.sh

---
+++ pumpanddump.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo pkil -9 docker
+sudo service docker stop
+cd /opt/plex/Library/Application\ Support/Plex\ Media\ Server/Plug-in\ Support/Databases/
+cp com.plexapp.plugins.library.db com.plexapp.plugins.library.db.original
+sqlite3 com.plexapp.plugins.library.db "DROP index 'index_title_sort_naturalsort'"
+sqlite3 com.plexapp.plugins.library.db "DELETE from schema_migrations where version='20180501000000'"
+sqlite3 com.plexapp.plugins.library.db .dump > dump.sql
+rm com.plexapp.plugins.library.db
+sqlite3 com.plexapp.plugins.library.db < dump.sql
+sqlite3 com.plexapp.plugins.library.db "pragma synchronous = normal"
+sqlite3 com.plexapp.plugins.library.db "pragma cache_size = 15000000"
+sqlite3 com.plexapp.plugins.library.db "pragma temp_store = memory"
+sqlite3 com.plexapp.plugins.library.db "pragma mmap_size = 30000000000"
+sqlite3 com.plexapp.plugins.library.db "pragma page_size = 32768"
+sqlite3 com.plexapp.plugins.library.db "pragma vacuum"
+sqlite3 com.plexapp.plugins.library.db "pragma optimize"
+cd ~/
+sudo service docker start
+/opt/docker/docker-compose down
+/opt/docker docker-compose up -d
