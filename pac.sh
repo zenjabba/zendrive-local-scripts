736d01ee1a5cb793d5815f8e4b9cbc08c918911c
hthighwaymonk <49250826+hthighwaymonk@users.noreply.github.com>
2020-12-28 15:03:35 -0800

    Create pac.sh
    script to run the Plex-Auto-Collection docker in `-rm -d` mode
    just pass it the name of the config.yml you want to use

---
+++ pac.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# pass either movies.yml or tv.yml
+/usr/bin/docker run --rm -it -d --network="docker_default" -v /opt/Plex-Auto-Collections/config:/config -v /mnt/unionfs/inbound/librarybase/collections/posters:/config/posters -e PLEXAPI_PLEXAPI_TIMEOUT=60 mza921/plex-auto-collections -u -c /config/$1
