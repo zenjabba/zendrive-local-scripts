287e36bcdb548d1d3674431140a43a06b23075b3
hthighwaymonk <49250826+hthighwaymonk@users.noreply.github.com>
2020-12-31 15:20:28 -0800

    Update pac.sh

--- pac.sh
+++ pac.sh
@@ -3,4 +3,4 @@
 # https://github.com/mza921/Plex-Auto-Collections
 # script uses the pooster folder in zd-inbound
 # pass either movies.yml or tv.yml
-/usr/bin/docker run --rm -it -d --network="docker_default" -v /opt/Plex-Auto-Collections/config:/config -v /mnt/unionfs/inbound/librarybase/collections/posters:/config/posters -e PLEXAPI_PLEXAPI_TIMEOUT=60 mza921/plex-auto-collections -u -c /config/$1
+/usr/bin/docker run --rm -it -d --network="docker_default" --name pac -v /opt/Plex-Auto-Collections/config:/config -v /mnt/unionfs/inbound/librarybase/collections/posters:/config/posters -e PLEXAPI_PLEXAPI_TIMEOUT=60 mza921/plex-auto-collections -u -c /config/$1
