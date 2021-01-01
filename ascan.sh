4020df34a61dedc7c750e51ac33b11f5e6da728d
hthighwaymonk <49250826+hthighwaymonk@users.noreply.github.com>
2020-12-31 20:47:54 -0800

    Update ascan.sh
    Updated to use the new manual trigger in autoscan 1.0

--- ascan.sh
+++ ascan.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-# Helper script for manual scans with Cloudbox autoscan
+# Helper script for manual scans with Cloudbox autoscan 1.0 OR above
+# uses the new manual scan function
 
 function usage {
   echo ""
-  echo "Usage: ascan \"Path to media\" mediaType"
+  echo "Usage: ascan.sh \"Path to media\"
   echo ""
   echo "Examples:"
-  echo "    ascan \"/mnt/unionfs/Media/TV/Series/Season 1/Episode 01.mkv\" tv"
-  echo "    ascan \"/mnt/unionfs/Media/Movies/Movie Folder/Movie.mkv\" movie"
+  echo "    ascan.sh \"/mnt/unionfs/Media/TV/Series/Season 1/Episode 01.mkv\"
+  echo "    ascan.sh \"/mnt/unionfs/Media/Movies/Movie Folder/Movie.mkv\"
   exit 1
 }
 
@@ -16,29 +17,6 @@ if [ -z "$1" ]; then
   usage
 fi
 
-case $2 in
-  movie)
-      arrType="radarr"
-      folderPath=$(dirname "$1")
-      relativePath=$(basename "$1")
-      jsonData='{"eventType": "Download", "movie": {"folderPath": "'"$folderPath"'"}, "movieFile": {"relativePath": "'"$relativePath"'"}}'
-      ;;
-  tv|television|series)
-      arrType="sonarr"
-      folderPath=$(dirname "$(dirname "$1")")
-      relativePath="$(basename "$(dirname "$1")")/$(basename "$1")"
-      jsonData='{"eventType": "Download","episodeFile": {"relativePath": "'"$relativePath"'"},"series": {"path": "'"$folderPath"'"}}'
-      ;;
-  '')
-      echo "Media type parameter is empty"
-      usage
-      ;;
-  *)
-      echo "Media type specified unknown"
-      usage
-      ;;
-esac
-
-curl -d "$jsonData" -H "Content-Type: application/json" http://127.0.0.1:3030/triggers/$arrType -u username:password > /dev/null
+curl -G --request POST --url 'http://127.0.0.1:3030/triggers/manual' --data-urlencode '$1' > /dev/null
 if [ $? -ne 0 ]; then echo "Unable to reach autoscan ERROR: $?";fi
 echo "$1 added to your autoscan queue!"
