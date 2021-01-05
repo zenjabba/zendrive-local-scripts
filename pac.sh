a02fb61d709818321d471f547657ddab5748bd94
hthighwaymonk <49250826+hthighwaymonk@users.noreply.github.com>
2021-01-05 11:09:02 -0800

    Update pac.sh

--- pac.sh
+++ pac.sh
@@ -2,6 +2,9 @@
 # runs Plex-Auto-Collections -
 # https://github.com/mza921/Plex-Auto-Collections
 # script uses the poster folder in zd-inbound
-# pass either movies.yml or tv.yml
-# as in:  pac.sh movies.yml
-/usr/bin/docker run --rm -it -d --network="docker_default" --name pac -v /opt/Plex-Auto-Collections/config:/config -v /mnt/unionfs/inbound/librarybase/collections/posters:/config/posters -e PLEXAPI_PLEXAPI_TIMEOUT=60 mza921/plex-auto-collections -u -c /config/$1
+# there are sample config.yml files in /mnt/unionfs/inbound/librarybase/collections/
+# pass either movies.yml or tv.yml (examples)
+# as in:  ./pac.sh movies.yml
+# if you need/want to run this as a specific user in docker add something like --user seed:seed 
+/bin/mkdir -P /opt/pac
+/usr/bin/docker run --rm -it -d --network="docker_default" --name pac -v /opt/pac:/config -v /mnt/unionfs/inbound/librarybase/collections/posters:/config/posters -e PLEXAPI_PLEXAPI_TIMEOUT=60 mza921/plex-auto-collections -u -c /config/$1
