5b571e46eace99139557140e0c651eead3f8c30d
hthighwaymonk <49250826+hthighwaymonk@users.noreply.github.com>
2021-01-10 21:00:11 -0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,13 +3,14 @@
 # $1 = folder where your docker-compose.yml is stored
 # $2 = name of source snapshot folder
 # $3 = name of destination snapshot folder
-# $4 = if your BTFS is at / and not /opt then set this to 1, otherwise leave empty
-#
+# $4 = Name of the Google Remote to use for upload
+# $5 = if your BTFS is at / and not /opt then set this to 1, otherwise leave empty
+# 
 # sample if you use BTRFS for /opt - 
-# ./backup.sh /opt/docker/ /opt /opt/snapshot
+# ./backup.sh /opt/docker/ /opt /opt/snapshot GOOGLE
 #
 # sample if you use BTRFS for / -
-# ./backup.sh /opt/docker/ / /opt/snapshot 1
+# ./backup.sh /opt/docker/ / /opt/snapshot GOOGLE 1
 #
 #   Make sure folders exist
 #
@@ -47,9 +48,10 @@ sudo systemctl start poller.service
 #
 # create tar files of each folder under /opt
 #
-cd $3
-if [ "$4" -eq "1" ]; then
+if [ "$5" -eq "1" ]; then
         cd $3/opt
+else
+        cd $3
 fi
 /usr/bin/find . -maxdepth 1 -mindepth 1 -type d -exec tar cvf /mnt/local/backup/{}.tar {}  \;
 wait
@@ -60,5 +62,5 @@ sudo btrfs subvolume delete $3
 #
 # upload to GDrive
 # 
-rclone move /mnt/local/backup/ GOOGLE:/Backups/$(date +"%Y-%m-%d")/ --transfers=50 -vvP
+rclone move /mnt/local/backup/ $4:/Backups/$(date +"%Y-%m-%d")/ --transfers=50 -vvP
 wait
