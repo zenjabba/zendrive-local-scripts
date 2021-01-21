8f07b6ef856ed993d3399415b59bab8175f3a5b6
Paul Hawkins <hthighway@gmail.com>
2021-01-20 17:40:58 -0800

    removed ascan

--- resend_poller.sh
+++ resend_poller.sh
@@ -46,6 +46,5 @@ do
    val=${i2//\"/}
    curl -G --request POST --url "http://127.0.0.1:3030/triggers/manual" --data-urlencode "dir=${val}"
    if [ $? -ne 0 ]; then echo "Unable to reach autoscan ERROR: $?";fi
-   if [ $? -eq 0 ]; then echo "$1 added to your autoscan queue!";fi
    sleep 5
 done
