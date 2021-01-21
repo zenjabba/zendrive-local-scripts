0cb1e6437a51da9063a8d997aa62403b01178201
Paul Hawkins <hthighway@gmail.com>
2021-01-20 16:54:58 -0800

    added hours ago

--- resend_poller.sh
+++ resend_poller.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 #
 # install JQ if not installed
-if hash jq 2> /dev/null; then echo "OK, you have jq installed. We’ll use that."; else sudo apt install jq -y; fi
+if hash jq 2> /dev/null; then echo "OK, you have jq installed. We ^`^yll use that."; else sudo apt install jq -y; fi
 #
 # point this to where your poller log sits
 logfile=/var/log/poller.log
 #
+#   get the hours ago value from $1
+hoursago="${1} hours ago"
+d1=$(date -d "${hoursago}" "+%Y-%m-%dT%H:%M:%SZ")
+d1=$(date -d "${d1}" +%s)
 #
-checkfor="using custom autoscan trigger"
-
-#d1=$(date -d '$1 hour ago' "+%Y/%m/%d:%H:%M:%S")
-#d1=$(date -d $d1 +%s)
-
-for log in $logfile
+declare -a values
+declare -a valuelist
+mapfile -t values <<< $(jq -c '. | select(.message|test("custom."))' "$logfile")
+for i in "${values[@]}"
 do
-   check=$(echo "$log" |  jq -r  '.message')
-   if [[ $log == *"${checkfor}"* ]]; then
-      #d2=$(echo "$var" |  jq -r  '.time')
-      #d2=$(date -d $d2 +%s)
-      #if [ $d2 > $d1 ]; then
-      var=$(echo "$log" |  jq -r  '.path')
-      varlist=("${var}")
-      echo "${var}"
-   fi
+    d2=$(echo "${i}" |  jq -r  '.time')
+    d2=$(date -d "${d2}" +%s)
+    if [[ ${d2} -ge ${d1} ]]; then
+       var=$(echo "${i}" |  jq -r  '.path')
+       var=${var#*/}
+       var="$(echo -e "${var}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+       file_list+=("${var}")
+    fi 
 done
-#IFS=$'\n'
-#readarray -t uniq < <(printf '%s\n' "${varlist[@]}" | sort -u)
-#unset IFS
-#for i2 in "${uniq[@]}"; 
-#do 
-#   val=${i2//\"/}
-#   /opt/scripts/ascan.sh "/mnt/unionfs${val}/"
-#   sleep 5
-#done
+IFS=$'\n'
+readarray -t uniq < <(printf '%s\n' "${file_list[@]}" | sort -u)
+unset IFS
+for i2 in "${uniq[@]}"; 
+do 
+   val=${i2//\"/}
+   /opt/scripts/ascan.sh "/mnt/unionfs${val}/"
+   sleep 5
+done
