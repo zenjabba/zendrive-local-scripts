8e1338c63bbdc2d48a483beefac73dc7eb0beaf5
Jonathan Finley <accounts@jonathanfinley.net>
2021-01-28 00:57:47 -0500

    add extra tweaks and clean up pump and dump

--- fixdatabase.sh
+++ fixdatabase.sh
@@ -11,5 +11,8 @@ plexdocker="plex"
     sqlite3 "$plexdb" "pragma page_size=32768; vacuum;"
     sqlite3 "$plexdb" "pragma default_cache_size=20000000"
     sqlite3 "$plexdb" < /tmp/dump.sql
+    sqlite3 "$plexdb" "UPDATE metadata_items SET added_at = originally_available_at WHERE added_at <> originally_available_at AND originally_available_at IS NOT NULL;"
+    sqlite3 "$plexdb" "UPDATE metadata_items SET added_at = DATETIME('now', '-1 days') WHERE DATETIME(added_at) > DATETIME('now');"
+    sqlite3 "$plexdb" "UPDATE metadata_items SET added_at = DATETIME('now', '-1 days') WHERE DATETIME(originally_available_at) > DATETIME('now');"
     chown seed:seed "$plexdb"
     docker start "${plexdocker}"
