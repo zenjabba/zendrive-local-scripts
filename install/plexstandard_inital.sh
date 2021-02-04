091d44a3500b6d43b40ccd9e0d5641d1abee52d2
zenjabba <zenjabba@users.noreply.github.com>
2021-02-04 16:22:23 -0500

    Update plexstandard_inital.sh

--- install/plexstandard_inital.sh
+++ install/plexstandard_inital.sh
@@ -22,6 +22,20 @@ fi
 ACCESS_KEY_ID="$1"       
 SECRET_ACCESS_KEY="$2"   
 
+##Ceph Setup
+#/bin/bash
+mkdir -p /etc/ceph
+echo "deb https://download.ceph.com/debian-octopus/ bionic main" >> /etc/apt/sources.list.d/ceph.list
+echo "[global]
+        fsid = ae0bd11a-6639-11eb-9fa4-0050569051a7
+        mon_host = [v2:10.10.3.22:3300/0,v1:10.10.3.22:6789/0] [v2:10.10.3.11:3300/0,v1:10.10.3.11:6789/0] [v2:10.10.3.12:3300/0,v1:10.10.3.12:6789/0] [v2:10.10.3.13:3300/0,v1:10.10.3.13:6789/0] [v2:10.10.3.14:3300/0,v1:10.10.3.14:6789/0]" > /etc/ceph/ceph.conf
+wget https://download.ceph.com/keys/release.asc
+sudo apt-key add release.asc
+rm release.asc
+sudo apt update
+sudo apt remove ceph-common -y
+sudo apt install ceph-common -y
+
 
 ##Shell Setup
 sudo apt install -y apache2-utils bwm-ng cifs-utils git htop intel-gpu-tools iotop iperf3 ncdu nethogs nload psmisc python3-pip python-pip screen sqlite3 tmux tree unrar-free vnstat wget zsh
