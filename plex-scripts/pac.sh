6b8370501c7f6a407cc0c3cff9a38953bc0a82d4
hthighwaymonk <49250826+hthighwaymonk@users.noreply.github.com>
2021-02-08 12:45:43 -0800

    Update pac.sh
    change -P to -p in the mkdir line

--- plex-scripts/pac.sh
+++ plex-scripts/pac.sh
@@ -10,7 +10,7 @@
 # if you need/want to run this as a specific user in docker add something like --user seed:seed 
 #
 #
-/bin/mkdir -P /opt/pac
+/bin/mkdir -p /opt/pac
 /usr/bin/rclone -vvv rc vfs/refresh dir=/zd-inbound/inbound/librarybase/collections recursive=false --rc-addr=localhost:$2
 /usr/bin/rclone -vvv rc vfs/refresh dir=/zd-inbound/inbound/librarybase/collections recursive=true --rc-addr=localhost:$2
 /usr/bin/docker run --rm -it -d --network="docker_default" --name pac -v /opt/pac:/config -v /mnt/unionfs/inbound/librarybase/collections/posters:/config/posters -e PLEXAPI_PLEXAPI_TIMEOUT=60 mza921/plex-auto-collections -u -c /config/$1
