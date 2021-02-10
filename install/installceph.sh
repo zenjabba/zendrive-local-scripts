21265ff922aa05801c6b54b264e45ccafe5ef7a6
zenjabba <zenjabba@users.noreply.github.com>
2021-02-10 11:12:32 -0500

    Update installceph.sh

--- install/installceph.sh
+++ install/installceph.sh
@@ -1,7 +1,8 @@
 #/bin/bash
 mkdir -p /etc/ceph
 mkdir -p /mnt/sharedrives/zd-storage-ceph
-echo "deb https://download.ceph.com/debian-octopus/ bionic main" >> /etc/apt/sources.list.d/ceph.list
+version=$(lsb_release -a)
+echo "deb https://download.ceph.com/debian-octopus/ $version main" >> /etc/apt/sources.list.d/ceph.list
 echo "[global]
         fsid = ae0bd11a-6639-11eb-9fa4-0050569051a7
         mon_host = [v2:10.10.3.22:3300/0,v1:10.10.3.22:6789/0] [v2:10.10.3.11:3300/0,v1:10.10.3.11:6789/0] [v2:10.10.3.12:3300/0,v1:10.10.3.12:6789/0] [v2:10.10.3.13:3300/0,v1:10.10.3.13:6789/0] [v2:10.10.3.14:3300/0,v1:10.10.3.14:6789/0]" > /etc/ceph/ceph.conf
