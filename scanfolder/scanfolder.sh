ddac9300e8aac02fd91e5e97c8c60b85ee29bbd8
hthighwaymonk <49250826+hthighwaymonk@users.noreply.github.com>
2021-02-12 10:08:54 -0800

    Update scanfolder.sh
    Removed the use of rclone and replaced it with FIND
    this is for the CEPH switch over

--- scanfolder/scanfolder.sh
+++ scanfolder/scanfolder.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-# /path/scanfolder/scanfolder.sh -s tv/10s -c /mnt/unionfs/ -t tv -u http://autoscan.TDL:3030 -p usernamepassword -o plex -z '/path to plex db/' -w 10 -r zendrive -a zd-tv2 -j 5577 -v 1 
+# /path/scanfolder/scanfolder.sh -s tv/10s -c /mnt/unionfs/ -t tv -u http://autoscan.TDL:3030 -p usernamepassword -o plex -z '/path to plex db/' -w 10 
 #-w = second to wait between sends to autoscan
-#-r = RCLONE mount, like zendrive or zd_storage
-#-a = the folder name at the base of the mount: zd-movies,zd-tv1,zd-tv2,zd-tv3
 #-d = integer for number of days
 #-h = integer for number of hours
 #-l = path to autoscan.db /your/path/
 # do not use both -d & -h - please just one
-#-j thr rclone rc port number you use
-# -v set to 1 if you use VFS Cache in your rclone mount
 
-while getopts s:c:t:u:p:o:z:w:r:a:d:h:l:j:v: option; do 
+while getopts s:c:t:u:p:o:z:w:d:h:l: option; do 
     case "${option}" in
         s) SOURCE_FOLDER=${OPTARG};;
         c) CONTAINER_FOLDER=${OPTARG};;
@@ -20,21 +16,14 @@ while getopts s:c:t:u:p:o:z:w:r:a:d:h:l:j:v: option; do
         o) DOCKERNAME=${OPTARG};;
         z) PLEXDB=${OPTARG};;
         w) WAIT=${OPTARG};;
-        r) RCLONEMOUNT=${OPTARG};;
-        a) ZDTD=${OPTARG};;
         d) DAYS=${OPTARG};;
         h) HOURS=${OPTARG};;
         l) ASCAN=${OPTARG};;
-        j) RCPORT=${OPTARG};; 
-        v) USEVFS=${OPTARG};; 
-               
      esac
 done
 
 get_files ()
 {
-  rclone_refresh "$RCPORT" "$ZDTD/$SOURCE_FOLDER"
-     
   case $TRIGGER in
           movie)
                   depth=2
@@ -60,19 +49,19 @@ get_files ()
   fi
   if [ ! -z "${DAYS}" ] && [ -z "${HOURS}" ]; then
     IFS=$'\n' 
-    filelist=($(rclone lsf --files-only --absolute --max-age "${DAYS}d" --max-depth "$depth" --format pt --separator "|" "$RCLONEMOUNT:$ZDTD/$SOURCE_FOLDER"))
+    filelist=($(find ${CONTAINER_FOLDER}${SOURCE_FOLDER} R -type f -maxdepth "${depth}" -mtime +"${DAYS}" -printf "%p|%T+\n" | sort))
     unset IFS
     MAXAGE=1
   fi
   if [ -z "${DAYS}" ] && [ ! -z "${HOURS}" ]; then
     IFS=$'\n' 
-    filelist=($(rclone lsf --files-only --absolute --max-age "${HOURS}h" --max-depth "$depth" --format pt --separator "|" "$RCLONEMOUNT:$ZDTD/$SOURCE_FOLDER"))
+    filelist=($(find ${CONTAINER_FOLDER}${SOURCE_FOLDER} -type f -maxdepth "${depth}" -mtime +"${DAYS}" -printf "%p|%T+\n" | sort))
     unset IFS
     MAXAGE=1
   fi
   if [ -z ${MAXAGE+x} ]; then
      IFS=$'\n' 
-    filelist=($(rclone lsf --files-only --absolute --max-depth "$depth" --format pt --separator "|" "$RCLONEMOUNT:$ZDTD/$SOURCE_FOLDER"))
+    filelist=($(find ${CONTAINER_FOLDER}${SOURCE_FOLDER} -type f -maxdepth "${depth}" -printf "%p|%T+\n" | sort))
     unset IFS
   fi
   for i in "${filelist[@]}"
@@ -130,62 +119,6 @@ process_autoscan () {
               sleep "$WAIT"
           fi
         fi
-}
-
-rclone_refresh ()
-{
-#set recurse = false for selected folder
-echo "beginning vfs/refresh recursive=false of ${2}"
-VAR=$(/usr/bin/rclone rc vfs/refresh --rc-addr=localhost:"$1" _async=true recursive=false dir="$2" | grep "jobid")
-JID=${VAR#*:}
-JID=$(echo -e "${JID}" | tr -d '[:space:]')
-VAR2=$(/usr/bin/rclone rc --rc-addr=:"$1" job/status jobid=${JID} | grep "success")
-value=${VAR2#*:}
-value=$(echo -e "${value}" | tr -d '[:space:]')
-while [ "$value" != "true" ]; do
-  VAR2=$(/usr/bin/rclone rc --rc-addr=:"$1" job/status jobid=${JID} | grep "success")
-  value=${VAR2#*:}
-  value=$(echo -e "${value}" | tr -d '[:space:]')
-  sleep 1
-done
-
-
-# if recursive false retuns OK, then continue with recursive true
-CHECK=$(/usr/bin/rclone rc --rc-addr=:"$1" job/status jobid=${JID} | grep "$2")
-CHECK=${CHECK:(-4)}
-CHECK=${CHECK//\"/}
-if [ "$CHECK" = "OK" ]; then
-   echo "vfs/refresh recursive=false of ${2} completed"
-   if [ "$USEVFS" != "1" ]; then
-    echo "No VFS Cache so NO recursive=true"
-   else
-       echo "beginning vfs/refresh recursive=true of ${2}"
-       VAR=$(/usr/bin/rclone rc vfs/refresh --rc-addr=localhost:"$1" _async=true recursive=true dir="$2" | grep "jobid")
-       JID=${VAR#*:}
-       JID=$(echo -e "${JID}" | tr -d '[:space:]')
-       VAR2=$(/usr/bin/rclone rc --rc-addr=:"$1" job/status jobid=${JID} | grep "success")
-       value=${VAR2#*:}
-       value=$(echo -e "${value}" | tr -d '[:space:]')
-       while [ "$value" != "true" ]; do
-         VAR2=$(/usr/bin/rclone rc --rc-addr=:"$1" job/status jobid=${JID} | grep "success")
-         value=${VAR2#*:} 
-         value=$(echo -e "${value}" | tr -d '[:space:]')
-         sleep 1
-       done
-       CHECK=$(/usr/bin/rclone rc --rc-addr=:"$1" job/status jobid=${JID} | grep "$2")
-       CHECK=${CHECK:(-4)}
-       CHECK=${CHECK//\"/}
-       if [ "$CHECK" = "OK" ]; then
-        echo "vfs/refresh recursive=true of ${2} completed"
-       else
-         echo "vfs/refresh recursive=true of ${2} failed, exiting script"
-         exit
-       fi
-    fi
-else
-   echo "vfs/refresh recursive=false of ${2} failed, exiting script"
-   exit
-fi
 }
 
 autoscan_check ()
