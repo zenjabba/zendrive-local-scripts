1fc625bf80519b46c13e38db3c4fc780e3ca57ee
zenjabba <zenjabba@users.noreply.github.com>
2021-02-12 11:29:57 -0500

    more updates

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ shellsetup() {
     wget -O /usr/local/bin/ https://raw.githubusercontent.com/pimlie/ubuntu-mainline-kernel.sh/master/ubuntu-mainline-kernel.sh
     chmod +x /usr/local/bin/ubuntu-mainline-kernel.sh
     sudo /usr/local/bin/ubuntu-mainline-kernel.sh -i 5.10.1
+    groupadd -g 32574 curators -r
 }
 
 ### install-ceph
@@ -25,11 +26,8 @@ installceph () {
 mkdir -p /etc/ceph
 mkdir -p /mnt/sharedrives/zd-storage-ceph
 echo "deb https://download.ceph.com/debian-octopus/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/ceph.list
-echo "[global]
-        fsid = ae0bd11a-6639-11eb-9fa4-0050569051a7
-        mon_host = [v2:10.10.3.22:3300/0,v1:10.10.3.22:6789/0] [v2:10.10.3.11:3300/0,v1:10.10.3.11:6789/0] [v2:10.10.3.12:3300/0,v1:10.10.3.12:6789/0] [v2:10.10.3.13:3300/0,v1:10.10.3.13:6789/0] [v2:10.10.3.14:3300/0,v1:10.10.3.14:6789/0]" > /etc/ceph/ceph.conf
-echo "[client.guest]
-        key = AQBWHhxgeRcTBRAAeLsjFI134LB6ZnZCoWQNKw==" > /etc/ceph/ceph.client.guest.keyring
+ln -s /opt/scripts/zendrive/ceph.conf /etc/ceph/ceph.conf
+ln -s /opt/scripts/zendrive/ceph-client-guest.keyring /etc/ceph/ceph.client.guest.keyring
 echo ":/ /mnt/sharedrives/zd-storage-ceph ceph name=guest,noatime,_netdev 0 2" >> /etc/fstab
 wget -q -O- 'https://download.ceph.com/keys/release.asc' | sudo apt-key add -
 sudo apt update -y
@@ -155,11 +153,11 @@ message() {
 }
 
 main() {
+    reposetup
     shellsetup
     foldersetup
     installceph
     usersetup
-    reposetup
     samplesetup
     scriptsetup
     dockersetup
